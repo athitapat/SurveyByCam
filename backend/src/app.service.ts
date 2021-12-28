0fa6057f9e34bbaaec7ec961a4de5f25550cd624
Athitpat <e4wearth@gmail.com>
2021-12-28 11:34:11 +0700

    Spawn Python process

--- backend/src/app.service.ts
+++ backend/src/app.service.ts
@@ -8,26 +8,27 @@ export class AppService {
 
   async pythonCombine(): Promise<string>{
     const spawn =  await require("child_process").spawn;
-      const pythonProcess =  await spawn('python', ["../Python/nodejs_combine.py"])
+      const pythonProcess =  await spawn('python', ["../Python/extract_text.py"])
       let mystr
       let myjson
       await pythonProcess.stdout.on('data', (data) => {
         console.log(`Node JS got Data ${data}`)
+        mystr = data
+        // mystr = data.toString();
+        // console.log(mystr)
 
-        mystr = data.toString();
-        console.log(mystr)
-
-        myjson = JSON.parse(mystr);
-        console.log(mystr)
+        // myjson = JSON.parse(mystr);
+        // console.log(mystr)
 
       })
       
-      pythonProcess.on('close', (code) => {
+      await pythonProcess.on('error', (error => console.log(`error: ${error.massge}`)))
+        pythonProcess.on('close', (code) => {
         console.log(`child process close all stdio with code ${code}`);
         // send data to browser
         //res.send(dataToSend)
         });
-      
-      return "test"
+      await console.log(mystr)
+      return mystr
   }
 }
